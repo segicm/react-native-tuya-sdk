041663d186a0e828659950b04a342739590b388a
Yakiv Vakoliuk <yahacomua@gmail.com>
2024-05-09 14:50:22 +0300

    app-2048: addDeviceListener promise reject changes

--- src/deviceSubscriptions.ts
+++ src/deviceSubscriptions.ts
@@ -22,11 +22,7 @@ type TDeviceListenerOptions = {
 }
 export const addDeviceListener = ({ devId, type}: TDeviceListenerOptions, cb: (data: unknown) => void, onError?: (e: unknown) => void) => {
   if (!subscriptions[devId]) {
-    try {
-      tuya.registerDevListener({ devId });
-    } catch (e) {
-      onError?.(e);
-    }
+    tuya.registerDevListener({ devId }).catch((e: unknown) => onError?.(e));
     subscriptions[devId] = 0;
   }
   const sub = addEvent(bridge(DEVLISTENER, devId), data => {
